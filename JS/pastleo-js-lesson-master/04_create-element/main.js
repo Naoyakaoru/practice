749a62f38faea143f3e376d372fd09d1aa6eaede
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-06 15:50:19 +0800

    04 fin

--- JS/pastleo-js-lesson-master/04_create-element/main.js
+++ JS/pastleo-js-lesson-master/04_create-element/main.js
@@ -7,13 +7,25 @@ window.addEventListener('DOMContentLoaded', function() {
     
     const currentTime = new Date();
     timeDiv.textContent = currentTime.toString()
-    document.querySelector('.right').appendChild(timeDiv)
+    document.querySelector('.right').appendChild(timeDiv) //放在該父層底下的最後一個位子 = beforeend
   })
   document.getElementById('reset-inner-html').addEventListener('click', function() {
     document.querySelector('.right').innerHTML = '<div class="title">DOM practice...</div>'
   })
   document.getElementById('remove').addEventListener('click', function() {
-    document.querySelector('.right .time:last-child').remove()
+    const lastTime = document.querySelector('.right .time:last-child')
+    if (lastTime) { //這樣若lastTime = null時才不會噴錯（無法remove）
+      lastTime.remove()
+    }
+  })
+  document.getElementById('create-element-insert-adjacent').addEventListener('click', function() {
+    const timeDiv = document.createElement('div')
+
+    timeDiv.className = 'time'
+
+    const currentTime = new Date()
+    timeDiv.textContent = currentTime.toString()
+    document.querySelector('.title').insertAdjacentElement('afterend', timeDiv)
   })
 
 })
