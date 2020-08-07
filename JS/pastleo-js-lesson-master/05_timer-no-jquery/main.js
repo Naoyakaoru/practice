f41fbaf47f4dabd5e93baa164d55417f662051ec
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-07 18:35:25 +0800

    hw fin

--- JS/pastleo-js-lesson-master/05_timer-no-jquery/main.js
+++ JS/pastleo-js-lesson-master/05_timer-no-jquery/main.js
@@ -2,12 +2,14 @@ window.addEventListener('DOMContentLoaded', function(){
   let cnt = 0, running = false, timer
   const btn = document.querySelector('button')
   const seconds = document.getElementById('seconds') 
+  const run_tag = document.querySelector('#running')
 
   btn.addEventListener('click', function(){
     if(running){
       btn.textContent = 'Start'
       running = false
       clearInterval(timer)
+      run_tag.style.height = '0' //若點擊停止，則run_tag消失
     } else {
       btn.textContent = 'Stop'
       running = true
@@ -16,6 +18,7 @@ window.addEventListener('DOMContentLoaded', function(){
         seconds.textContent = (Math.floor(cnt / 10))
         document.querySelector('.dots').insertAdjacentElement('afterbegin', document.querySelector('.dots>div:last-child'))
       },100)
+      run_tag.style.height = null
     }
   })
 })
