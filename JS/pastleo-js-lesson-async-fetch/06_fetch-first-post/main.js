020f0e3bcfe83ba240a496fe50460e784fa0a524
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-20 15:12:47 +0800

    async

--- JS/pastleo-js-lesson-async-fetch/06_fetch-first-post/main.js
+++ JS/pastleo-js-lesson-async-fetch/06_fetch-first-post/main.js
@@ -1,1 +1,19 @@
-// let's start coding:
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://pastleo-posts-api.herokuapp.com/api/posts')
+    .then(request => request.json()) //拿到.json的format
+    .then(posts => { //
+      console.log(posts)
+      const post = posts[0]
+
+      document.querySelector('.post-created-at').textContent = post.created_at
+      document.querySelector('.post-author').textContent = post.author
+      document.querySelector('.post-title').textContent = post.title
+      document.querySelector('.post-description').textContent = post.description
+
+      document.querySelector('.post-title').href = post.url
+      document.querySelector('.post-link').href = post.url
+      document.querySelector('.post-link').classList.remove('hidden') 
+
+    })
+
+})
--- JS/pastleo-js-lesson-async-fetch/07_async-restaurant/main.js
+++ JS/pastleo-js-lesson-async-fetch/07_async-restaurant/main.js
@@ -1,46 +1,83 @@
-const startTask = (orderDiv, name, ms, callback) => {
-  const stepP = document.createElement('p');
-  stepP.textContent = name;
-  orderDiv.appendChild(stepP);
-  if (callback && ms) {
+//old
+// const startTask = (orderDiv, name, ms, callback) => {
+//   const stepP = document.createElement('p');
+//   stepP.textContent = name;
+//   orderDiv.appendChild(stepP);
+//   if (callback && ms) {
+//     setTimeout(() => {
+//       callback();
+//     }, ms);
+//   }
+// }
+
+//new
+const startTask = (orderDiv, name, ms) => new Promise((resolve, reject) => {
+  const stepP = document.createElement('p')
+  stepP.textContent = name
+  orderDiv.appendChild(stepP)
+
+  if (ms) {
     setTimeout(() => {
-      callback();
-    }, ms);
+      resolve()
+    }, ms)
+  } else {
+    resolve()
   }
-}
+})
 
 document.addEventListener('DOMContentLoaded', () => {
-  const ordersDiv = document.querySelector('.orders');
+  const ordersDiv = document.querySelector('.orders')
 
   // 點熱牛奶
-  document.getElementById('order-hot-milk').addEventListener('click', () => {
-    const orderDiv = document.createElement('div');
-    orderDiv.className = 'p-4 border-dashed border-4 border-gray-600';
 
-    ordersDiv.appendChild(orderDiv);
+  document.getElementById('order-hot-milk').addEventListener('click', async () => {
+    const orderDiv = document.createElement('div')
+    orderDiv.className = 'p-4 border-dashed border-4 border-gray-600'
 
-    startTask(orderDiv, '接到熱牛奶訂單', 200, () => {
-      startTask(orderDiv, '把熱牛奶放入微波爐，開始微波', 1800, () => {
-        startTask(orderDiv, '微波完成，拿給消費者');
-      });
-    });
+    ordersDiv.appendChild(orderDiv)
+
+    await startTask(orderDiv, '接到熱牛奶訂單', 200)
+    await startTask(orderDiv, '把熱牛奶放入微波爐，開始微波', 1800)
+    await startTask(orderDiv, '微波完成，拿給消費者')
+
+
+    //old 因為startTask是一個promise, 所以可以直接接.then
+    // startTask(orderDiv, '接到熱牛奶訂單', 200).then(
+    //   () => startTask(orderDiv, '把熱牛奶放入微波爐，開始微波', 1800)
+    // ).then(
+    //   () => startTask(orderDiv, '微波完成，拿給消費者')
+    // )
+
+    //older
+    // startTask(orderDiv, '接到熱牛奶訂單', 200, () => {
+    //   startTask(orderDiv, '把熱牛奶放入微波爐，開始微波', 1800, () => {
+    //     startTask(orderDiv, '微波完成，拿給消費者');
+    //   });
+    // });
   });
 
   // 點漢堡
-  document.getElementById('order-hamburger').addEventListener('click', () => {
+  document.getElementById('order-hamburger').addEventListener('click', async () => {
     const orderDiv = document.createElement('div');
     orderDiv.className = 'p-4 border-dashed border-4 border-gray-600';
 
     ordersDiv.appendChild(orderDiv);
 
-    startTask(orderDiv, '接到漢堡訂單', 200, () => {
-      startTask(orderDiv, '準備麵包、生菜、漢堡肉', 600, () => {
-        startTask(orderDiv, '煎漢堡肉', 1500, () => {
-          startTask(orderDiv, '用麵包把漢堡肉跟生菜夾起來', 400, () => {
-            startTask(orderDiv, '上菜');
-          });
-        });
-      });
-    });
+    await startTask(orderDiv, '接到漢堡訂單', 200)
+    await startTask(orderDiv, '準備麵包、生菜、漢堡肉', 600)
+    await startTask(orderDiv, '煎漢堡肉', 1500)
+    await startTask(orderDiv, '用麵包把漢堡肉跟生菜夾起來', 400)
+    await startTask(orderDiv, '上菜')
+
+
+    // startTask(orderDiv, '接到漢堡訂單', 200, () => {
+    //   startTask(orderDiv, '準備麵包、生菜、漢堡肉', 600, () => {
+    //     startTask(orderDiv, '煎漢堡肉', 1500, () => {
+    //       startTask(orderDiv, '用麵包把漢堡肉跟生菜夾起來', 400, () => {
+    //         startTask(orderDiv, '上菜');
+    //       });
+    //     });
+    //   });
+    // });
   });
 });
